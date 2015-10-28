cfb9d374247d5ed70bc432e97a0dbea8aff1caa9
Sebastian Utz <su@rtme.net>
2015-10-28 12:25:37 +0100

    do crate specific template restore
    restore templates which matches restored indices
    do not overwrite existing templates during restore

--- core/src/main/java/org/elasticsearch/snapshots/RestoreService.java
+++ core/src/main/java/org/elasticsearch/snapshots/RestoreService.java
@@ -334,6 +334,9 @@ public class RestoreService extends AbstractComponent implements ClusterStateApp
                             }
                         }
 
+                        // restore templates which matches any restored index (but do NOT overwrite existing templates)
+                        restoreTemplatesMatchingRestoredIndices(mdBuilder, currentState);
+
                         shards = shardsBuilder.build();
                         RestoreInProgress.Entry restoreEntry = new RestoreInProgress.Entry(snapshot, overallState(RestoreInProgress.State.INIT, shards), Collections.unmodifiableList(new ArrayList<>(renamedIndices.keySet())), shards);
                         builder.putCustom(RestoreInProgress.TYPE, new RestoreInProgress(restoreEntry));
@@ -343,8 +346,8 @@ public class RestoreService extends AbstractComponent implements ClusterStateApp
 
                     checkAliasNameConflicts(renamedIndices, aliases);
 
-                    // Restore global state if needed
-                    restoreGlobalStateIfRequested(mdBuilder);
+                    // Restore global state if needed (but do NOT overwrite existing templates)
+                    restoreGlobalStateIfRequested(mdBuilder, currentState);
 
                     if (completed(shards)) {
                         // We don't have any indices to restore - we are done
@@ -451,7 +454,21 @@ public class RestoreService extends AbstractComponent implements ClusterStateApp
                     return builder.settings(Settings.builder().put(settingsMap)).build();
                 }
 
-                private void restoreGlobalStateIfRequested(MetaData.Builder mdBuilder) {
+                private void restoreTemplatesMatchingRestoredIndices(MetaData.Builder mdBuilder, ClusterState currentState) {
+                    if (metaData.templates() != null) {
+                        for (ObjectCursor<IndexTemplateMetaData> cursor : metaData.templates().values()) {
+                            for (String index : filteredIndices) {
+                                if (currentState.metaData().templates().get(cursor.value.name()) == null
+                                        && Regex.simpleMatch(cursor.value.template(), index)) {
+                                    mdBuilder.put(cursor.value);
+                                    break;
+                                }
+                            }
+                        }
+                    }
+                }
+
+                private void restoreGlobalStateIfRequested(MetaData.Builder mdBuilder, ClusterState currentState) {
                     if (request.includeGlobalState()) {
                         if (metaData.persistentSettings() != null) {
                             Settings settings = metaData.persistentSettings();
@@ -461,7 +478,9 @@ public class RestoreService extends AbstractComponent implements ClusterStateApp
                         if (metaData.templates() != null) {
                             // TODO: Should all existing templates be deleted first?
                             for (ObjectCursor<IndexTemplateMetaData> cursor : metaData.templates().values()) {
-                                mdBuilder.put(cursor.value);
+                                if (currentState.metaData().templates().get(cursor.value.name()) == null) {
+                                    mdBuilder.put(cursor.value);
+                                }
                             }
                         }
                         if (metaData.customs() != null) {
--- core/src/test/java/org/elasticsearch/snapshots/SharedClusterSnapshotRestoreIT.java
+++ core/src/test/java/org/elasticsearch/snapshots/SharedClusterSnapshotRestoreIT.java
@@ -498,6 +498,89 @@ public class SharedClusterSnapshotRestoreIT extends AbstractSnapshotIntegTestCas
 
     }
 
+    /**
+     * Test that templates which matches filtered indices are automatically restored
+     */
+    public void testRestoreTemplatesImplicitTest() throws Exception {
+        Client client = client();
+
+        logger.info("-->  creating repository");
+        assertAcked(client.admin().cluster().preparePutRepository("test-repo")
+                .setType("fs").setSettings(Settings.builder().put("location", randomRepoPath())));
+
+        logger.info("--> create test indices");
+        createIndex("test-idx-1", "test-idx-2", "test-idx-3");
+        ensureGreen();
+
+        logger.info("-->  creating test templates");
+        assertThat(client.admin().indices().preparePutTemplate("test-template").setTemplate("te*")
+            .addMapping("test-mapping", "{\"test-mapping\" : {\"properties\" : {\"v\" : {\"type\" : \"integer\"}}}}").get().isAcknowledged(), equalTo(true));
+        assertThat(client.admin().indices().preparePutTemplate("test-template-not-to-be-restored").setTemplate("foo*")
+            .addMapping("test-mapping-foo", "{\"test-mapping-foo\" : {\"properties\" : {\"v\" : {\"type\" : \"integer\"}}}}").get().isAcknowledged(), equalTo(true));
+
+        logger.info("--> snapshot");
+        CreateSnapshotResponse createSnapshotResponse = client.admin().cluster().prepareCreateSnapshot("test-repo", "test-snap").setIndices().setWaitForCompletion(true).get();
+        assertThat(createSnapshotResponse.getSnapshotInfo().totalShards(), greaterThan(0));
+        assertThat(createSnapshotResponse.getSnapshotInfo().successfulShards(), greaterThan(0));
+        assertThat(client.admin().cluster().prepareGetSnapshots("test-repo").setSnapshots("test-snap").get().getSnapshots().get(0).state(), equalTo(SnapshotState.SUCCESS));
+
+        logger.info("-->  delete test template");
+        assertThat(client.admin().indices().prepareDeleteTemplate("test-template").get().isAcknowledged(), equalTo(true));
+        assertThat(client.admin().indices().prepareDeleteTemplate("test-template-not-to-be-restored").get().isAcknowledged(), equalTo(true));
+        GetIndexTemplatesResponse getIndexTemplatesResponse = client().admin().indices().prepareGetTemplates().get();
+        assertIndexTemplateMissing(getIndexTemplatesResponse, "test-template");
+        assertIndexTemplateMissing(getIndexTemplatesResponse, "test-template-not-to-be-restored");
+
+        logger.info("--> delete indices");
+        cluster().wipeIndices("test-idx-1", "test-idx-2", "test-idx-3");
+
+        logger.info("--> restore cluster state");
+        RestoreSnapshotResponse restoreSnapshotResponse = client.admin().cluster().prepareRestoreSnapshot("test-repo", "test-snap")
+                .setIndices("test-idx-1", "test-idx-2", "test-idx-3").setWaitForCompletion(true).setRestoreGlobalState(false).execute().actionGet();
+
+        logger.info("--> check that indices are restored");
+        assertThat(restoreSnapshotResponse.getRestoreInfo().totalShards(), greaterThan(0));
+
+        logger.info("--> check that template is restored");
+        getIndexTemplatesResponse = client().admin().indices().prepareGetTemplates().get();
+        assertIndexTemplateExists(getIndexTemplatesResponse, "test-template");
+
+        logger.info("--> check that 2nd template is NOT restored");
+        assertIndexTemplateMissing(getIndexTemplatesResponse, "test-template-not-to-be-restored");
+    }
+
+    public void testRestoreTemplatesNotOverwritingExistingOnes() throws Exception {
+        Client client = client();
+
+        logger.info("-->  creating repository");
+        assertAcked(client.admin().cluster().preparePutRepository("test-repo")
+                .setType("fs").setSettings(Settings.builder().put("location", randomRepoPath())));
+
+        logger.info("-->  creating test template");
+        assertThat(client.admin().indices().preparePutTemplate("test-template").setTemplate("te*").addMapping("test-mapping", "{\"test-mapping\" : {\"properties\" : {\"old_value\" : {\"type\" : \"integer\"}}}}").get().isAcknowledged(), equalTo(true));
+
+        logger.info("--> snapshot");
+        CreateSnapshotResponse createSnapshotResponse = client.admin().cluster().prepareCreateSnapshot("test-repo", "test-snap").setIndices().setWaitForCompletion(true).get();
+        assertThat(createSnapshotResponse.getSnapshotInfo().totalShards(), equalTo(0));
+        assertThat(createSnapshotResponse.getSnapshotInfo().successfulShards(), equalTo(0));
+        assertThat(client.admin().cluster().prepareGetSnapshots("test-repo").setSnapshots("test-snap").get().getSnapshots().get(0).state(), equalTo(SnapshotState.SUCCESS));
+
+        logger.info("-->  update test template");
+        assertThat(client.admin().indices().preparePutTemplate("test-template").setTemplate("te*").addMapping("test-mapping", "{\"test-mapping\" : {\"properties\" : {\"value\" : {\"type\" : \"string\"}}}}").get().isAcknowledged(), equalTo(true));
+
+        logger.info("--> restore cluster state");
+        RestoreSnapshotResponse restoreSnapshotResponse = client.admin().cluster().prepareRestoreSnapshot("test-repo", "test-snap").setWaitForCompletion(true).setRestoreGlobalState(true).execute().actionGet();
+        // We don't restore any indices here
+        assertThat(restoreSnapshotResponse.getRestoreInfo().totalShards(), equalTo(0));
+
+        logger.info("--> check that template is NOT restored (already exists)");
+        GetIndexTemplatesResponse getIndexTemplatesResponse = client().admin().indices().prepareGetTemplates("test-template").get();
+        assertThat(getIndexTemplatesResponse.getIndexTemplates().size(), is(1));
+        assertThat(getIndexTemplatesResponse.getIndexTemplates().get(0).getMappings().size(), equalTo(1));
+        assertThat(getIndexTemplatesResponse.getIndexTemplates().get(0).getMappings().get("test-mapping").string(), equalTo("{\"test-mapping\" : {\"properties\" : {\"value\" : {\"type\" : \"string\"}}}}"));
+    }
+
+
     public void testIncludeGlobalState() throws Exception {
         Client client = client();
 
