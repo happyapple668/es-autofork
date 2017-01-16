0811778a546ae9aec6703b31381e980e0179dc3d
Sebastian Utz <su@rtme.net>
2017-01-16 16:13:31 +0100

    supports custom startup logging of Node child implementations
    this way we are able to log crate`s version/build instead of
    elasticsearch's version/build.
    also crate will have full control over what is logged at all.
    
    (cherry picked from commit c8df07e22d9dcdd21986c66eba7e33cee3940c8b)
    (cherry picked from commit 429715c973a8b56fe40774bf5b13b3983e571d16)

--- core/src/main/java/org/elasticsearch/node/Node.java
+++ core/src/main/java/org/elasticsearch/node/Node.java
@@ -268,38 +268,10 @@ public class Node implements Closeable {
                 throw new IllegalStateException("Failed to create node environment", ex);
             }
             final boolean hadPredefinedNodeName = NODE_NAME_SETTING.exists(tmpSettings);
-            Logger logger = Loggers.getLogger(Node.class, tmpSettings);
             final String nodeId = nodeEnvironment.nodeId();
             tmpSettings = addNodeNameIfNeeded(tmpSettings, nodeId);
             // this must be captured after the node name is possibly added to the settings
             final String nodeName = NODE_NAME_SETTING.get(tmpSettings);
-            if (hadPredefinedNodeName == false) {
-                logger.info("node name [{}] derived from node ID [{}]; set [{}] to override", nodeName, nodeId, NODE_NAME_SETTING.getKey());
-            } else {
-                logger.info("node name [{}], node ID [{}]", nodeName, nodeId);
-            }
-
-            final JvmInfo jvmInfo = JvmInfo.jvmInfo();
-            logger.info(
-                "version[{}], pid[{}], build[{}/{}], OS[{}/{}/{}], JVM[{}/{}/{}/{}]",
-                Version.displayVersion(Version.CURRENT, Build.CURRENT.isSnapshot()),
-                jvmInfo.pid(),
-                Build.CURRENT.shortHash(),
-                Build.CURRENT.date(),
-                Constants.OS_NAME,
-                Constants.OS_VERSION,
-                Constants.OS_ARCH,
-                Constants.JVM_VENDOR,
-                Constants.JVM_NAME,
-                Constants.JAVA_VERSION,
-                Constants.JVM_VERSION);
-            logger.info("JVM arguments {}", Arrays.toString(jvmInfo.getInputArguments()));
-            warnIfPreRelease(Version.CURRENT, Build.CURRENT.isSnapshot(), logger);
-
-            if (logger.isDebugEnabled()) {
-                logger.debug("using config [{}], data [{}], logs [{}], plugins [{}]",
-                    environment.configFile(), Arrays.toString(environment.dataFiles()), environment.logsFile(), environment.pluginsFile());
-            }
 
             this.pluginsService = new PluginsService(tmpSettings, environment.configFile(), environment.modulesFile(), environment.pluginsFile(), classpathPlugins);
             this.settings = pluginsService.updatedSettings();
@@ -309,6 +281,8 @@ public class Node implements Closeable {
             // this is just to makes sure that people get the same settings, no matter where they ask them from
             this.environment = new Environment(this.settings, environment.configFile());
             Environment.assertEquivalent(environment, this.environment);
+            Logger logger = Loggers.getLogger(Node.class, tmpSettings);
+            startUpLogging(logger, tmpSettings, nodeName, nodeId, hadPredefinedNodeName);
 
             final List<ExecutorBuilder<?>> executorBuilders = pluginsService.getExecutorBuilders(settings);
 
@@ -526,6 +500,37 @@ public class Node implements Closeable {
             if (!success) {
                 IOUtils.closeWhileHandlingException(resourcesToClose);
             }
+        }
+    }
+
+
+    protected void startUpLogging(Logger logger, Settings tmpSettings, String nodeName, String nodeId, boolean hadPredefinedNodeName) {
+        if (hadPredefinedNodeName == false) {
+            logger.info("node name [{}] derived from node ID [{}]; set [{}] to override", nodeName, nodeId, NODE_NAME_SETTING.getKey());
+        } else {
+            logger.info("node name [{}], node ID [{}]", nodeName, nodeId);
+        }
+
+        final JvmInfo jvmInfo = JvmInfo.jvmInfo();
+        logger.info(
+            "version[{}], pid[{}], build[{}/{}], OS[{}/{}/{}], JVM[{}/{}/{}/{}]",
+            Version.displayVersion(Version.CURRENT, Build.CURRENT.isSnapshot()),
+            jvmInfo.pid(),
+            Build.CURRENT.shortHash(),
+            Build.CURRENT.date(),
+            Constants.OS_NAME,
+            Constants.OS_VERSION,
+            Constants.OS_ARCH,
+            Constants.JVM_VENDOR,
+            Constants.JVM_NAME,
+            Constants.JAVA_VERSION,
+            Constants.JVM_VERSION);
+        logger.info("JVM arguments {}", Arrays.toString(jvmInfo.getInputArguments()));
+        warnIfPreRelease(Version.CURRENT, Build.CURRENT.isSnapshot(), logger);
+
+        if (logger.isDebugEnabled()) {
+            logger.debug("using config [{}], data [{}], logs [{}], plugins [{}]",
+                environment.configFile(), Arrays.toString(environment.dataFiles()), environment.logsFile(), environment.pluginsFile());
         }
     }
 
