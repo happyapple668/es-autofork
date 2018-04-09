amorvos/elasticsearch

allow to set different name for setting option
this is for CrateDB to allow the "-C" command line argument instead of
"-E"

core/src/main/java/org/elasticsearch/cli/EnvironmentAwareCommand.java
@@ -36,8 +36,14 @@ public abstract class EnvironmentAwareCommand extends Command {
     private final OptionSpec<KeyValuePair> settingOption;
 
     public EnvironmentAwareCommand(String description) {
+        this(description, "E");
+    }
+
+    public EnvironmentAwareCommand(String description, String settingOptionName) {
         super(description);
-        this.settingOption = parser.accepts("E", "Configure a setting").withRequiredArg().ofType(KeyValuePair.class);
+        this.settingOption = parser.accepts(settingOptionName, "Configure a setting")
+            .withRequiredArg()
+            .ofType(KeyValuePair.class);
     }
 
     @Override

happyapple668/es-autofork

(cherry picked from commit 19349c8c02d7e170df1415b09842ebda30bb0418)

core/src/main/java/org/elasticsearch/cli/EnvironmentAwareCommand.java
@@ -45,19 +45,18 @@ public abstract class EnvironmentAwareCommand extends Command {
      * @param description the command description
      */
     public EnvironmentAwareCommand(final String description) {
-        this(description, CommandLoggingConfigurator::configureLoggingWithoutConfig);
+        this(description, "E", CommandLoggingConfigurator::configureLoggingWithoutConfig);
     }
 
-    /**
-     * Construct the command with the specified command description and runnable to execute before main is invoked. Commands constructed
-     * with this constructor must take ownership of configuring logging.
-     *
-     * @param description the command description
-     * @param beforeMain the before-main runnable
-     */
-    public EnvironmentAwareCommand(final String description, final Runnable beforeMain) {
+    public EnvironmentAwareCommand(String description, Runnable beforeMain) {
+        this(description, "E", beforeMain);
+    }
+
+    public EnvironmentAwareCommand(String description, String settingOptionName, Runnable beforeMain) {
         super(description, beforeMain);
-        this.settingOption = parser.accepts("E", "Configure a setting").withRequiredArg().ofType(KeyValuePair.class);
+        this.settingOption = parser.accepts(settingOptionName, "Configure a setting")
+            .withRequiredArg()
+            .ofType(KeyValuePair.class);
     }
 
     @Override